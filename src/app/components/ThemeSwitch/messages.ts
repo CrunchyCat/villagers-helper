d96ffb2f293706649b00a9117b92836c0cd934ab
Caleb Hoff <crunchycat1@gmail.com>
2022-07-17 21:30:02 -0500

    Updates

--- src/app/components/ThemeSwitch/messages.ts
+++
@@ -1,6 +0,0 @@
-import { translations } from 'locales/translations'
-import { _t } from 'utils/messages'
-
-export const messages = {
-  selectTheme: () => _t(translations.themeSwitch.selectTheme, 'Select Theme')
-}
--- src/setupTests.ts
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom/extend-expect'
-
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
-
-import 'jest-styled-components'
