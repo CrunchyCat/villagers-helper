12b34be8307fccdd5b788e6dd947c999f8f6c0f8
Caleb Hoff <crunchycat1@gmail.com>
2022-07-29 18:44:18 -0500

    rollback sw worker notif test

--- src/index.tsx
+++ src/index.tsx
@@ -55,9 +55,4 @@ if (module.hot) {
 }
 
 // Register Service Worker for Offline Support
-//TODO: Better modals for update available & downloaded (including changelog)
-serviceWorkerRegistration.register({
-  onUpdate: () =>
-    setTimeout(() => alert('update available! restart to apply'), 500),
-  onCached: () => setTimeout(() => alert('just updated!'), 500)
-})
+serviceWorkerRegistration.register() //TODO: modals for update available & downloaded (including changelog)
