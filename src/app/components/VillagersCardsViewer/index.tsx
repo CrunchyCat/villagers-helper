74555bb0535bde4295d33903dc874eb7c3ffbeb8
Caleb Hoff <crunchycat1@gmail.com>
2022-08-16 20:43:35 -0500

    add viewbar to cards viewer

---
+++ src/app/components/VillagersCardsViewer/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import styled from 'styled-components/macro'
+import { ViewBar } from 'app/components/ViewBar'
+import { VillagersSet } from './VillagersSet'
+import { groups } from 'data/card/cards'
+import { useSelector } from 'react-redux'
+import { selectCardsFilters } from 'app/pages/CardsPage/slice/selectors'
+
+export const VillagersCardsViewer = React.memo(() => {
+  const filters = useSelector(selectCardsFilters)
+
+  return (
+    <Wrapper>
+      <ViewBar />
+      {Object.entries(groups[filters.group].sets)
+        .filter(x => !x[1]?.hide) // Ignore hidden sets
+        .map(([setID, set]) => (
+          <VillagersSet
+            setID={setID}
+            set={set}
+            query={filters.query.trim()}
+            view={filters.view}
+            editMode={false}
+            key={setID}
+          />
+        ))}
+    </Wrapper>
+  )
+})
+
+const Wrapper = styled.main`
+  max-width: 78rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  padding 0 0.2rem 0.5rem 0.2rem;
+`
--- src/app/pages/CardsPage/index.tsx
+++ src/app/pages/CardsPage/index.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
-import styled from 'styled-components/macro'
 import { Helmet } from 'react-helmet-async'
 import { NavBar } from 'app/components/Overlay/components/NavBar'
-import { ViewBar } from './components/ViewBar'
-import { VillagersCards } from 'app/components/VillagersCards'
+import { VillagersCardsViewer } from 'app/components/VillagersCardsViewer'
 import { useDispatch, useSelector } from 'react-redux'
 import { useCardsFiltersSlice } from 'app/pages/CardsPage/slice'
 import { selectCardsFilters } from 'app/pages/CardsPage/slice/selectors'
@@ -25,23 +23,7 @@ export const CardsPage = () => {
           dispatch(actions.setViewbarHidden(!filters.hideViewbar))
         }
       />
-      <Wrapper>
-        <ViewBar />
-        <VillagersCards
-          group={filters.group}
-          query={filters.query.trim()}
-          view={filters.view}
-          editMode={false}
-        />
-      </Wrapper>
+      <VillagersCardsViewer />
     </>
   )
 }
-
-const Wrapper = styled.main`
-  max-width: 78rem;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  padding 0 0.2rem 0.5rem 0.2rem;
-`
