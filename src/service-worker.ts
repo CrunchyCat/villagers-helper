9288abdd956caf1ae41a9bb931dbd64cfd2d27f5
Caleb Hoff <crunchycat1@gmail.com>
2022-08-21 17:46:36 -0500

    simplify service-worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -13,25 +13,17 @@ clientsClaim()
 // variable must be somewhere in the service worker file, even if not using precaching. See https://cra.link/PWA
 precacheAndRoute(self.__WB_MANIFEST)
 
-// Set up App Shell-style routing, so that all navigation requests are fulfilled with your index.html shell. Learn more
-// at https://developers.google.com/web/fundamentals/architecture/app-shell
+// Set up App Shell-style routing, so that all navigation requests are fulfilled with your index.html shell (if true)
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$')
-registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
-  ({ request, url }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') return false
+registerRoute(({ request, url }) => {
+  // If this isn't a navigation, skip.
+  if (request.mode !== 'navigate') return false
 
-    // If this is a URL that starts with /_, skip.
-    if (url.pathname.startsWith('/_')) return false
+  // If this looks like a URL for a resource, because it contains a file extension, skip.
+  if (url.pathname.match(fileExtensionRegexp)) return false
 
-    // If this looks like a URL for a resource, because it contains a file extension, skip.
-    if (url.pathname.match(fileExtensionRegexp)) return false
-
-    return true // Return true to signal that we want to use the handler.
-  },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
-)
+  return true // Return true to signal that we want to use the handler.
+}, createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'))
 
 // This allows the web app to trigger skipWaiting via registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', event => {
