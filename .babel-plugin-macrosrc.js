ae5fd8601136c2bb0b2afadd580f57af4d11adfc
Caleb Hoff <crunchycat1@gmail.com>
2022-08-28 22:20:49 -0500

    -866 bytes improving styled-components config

--- .babel-plugin-macrosrc.js
+++ .babel-plugin-macrosrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   styledComponents: {
-    displayName: process.env.NODE_ENV !== 'production'
+    displayName: process.env.NODE_ENV !== 'production',
+    ssr: false // Can cause errors during hydration
   }
 }
