a1fe0676dbbd4896092cd641a99f10535e96b264
Caleb Hoff <crunchycat1@gmail.com>
2022-08-29 19:21:30 -0500

    make app smaller

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@
  * This is the entry file for the application, only setup and boilerplate code.
  */
 
-import 'react-app-polyfill/stable'
+import 'core-js/stable'
 
 import * as React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
